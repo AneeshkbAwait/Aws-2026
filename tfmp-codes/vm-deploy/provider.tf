feba50e6e1eaea91d033f212d0c98535195fc58a
AneeshkbAwait <aneeshawaits@gmail.com>
2026-05-09 21:35:03 +0530

    feat: implement read secrets from aws secret manager

--- tfmp-codes/vm-deploy/provider.tf
+++ tfmp-codes/vm-deploy/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "ap-south-1"
-  access_key = " " # access key
-  secret_key = " " # secret
+  region     = var.region
+  access_key = var.my_access_key # access key
+  secret_key = var.my_secret_access_key # secret
 }
