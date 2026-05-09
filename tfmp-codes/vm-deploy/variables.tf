db50237565ca82767ad6dbdc9fc88b8729ae51ab
AneeshkbAwait <aneeshawaits@gmail.com>
2026-05-09 23:07:37 +0530

    feat: reading yaml config setup

--- tfmp-codes/vm-deploy/variables.tf
+++ tfmp-codes/vm-deploy/variables.tf
@@ -35,29 +35,29 @@ variable "my_secret_access_key"{
 #   }
 # }
 
-variable "instances" {
-  type = map(object({
-    instance_type = string
-    key_name      = string
-    tags          = map(string)
-  }))
-  default = {
-    vm1 = {
-      instance_type = "t3.micro"
-      key_name      = "tfm-key"
-      tags = {
-        Name    = "vm1"
-        Project = "zomato"
-      }
-    }
+# variable "instances" {
+#   type = map(object({
+#     instance_type = string
+#     key_name      = string
+#     tags          = map(string)
+#   }))
+#   default = {
+#     vm1 = {
+#       instance_type = "t3.micro"
+#       key_name      = "tfm-key"
+#       tags = {
+#         Name    = "vm1"
+#         Project = "zomato"
+#       }
+#     }
 
-    vm2 = {
-      instance_type = "t2.micro"
-      key_name      = "tfm-key"
-      tags = {
-        Name    = "vm2"
-        Project = "uber"
-      }
-    }
-  }
-}
+#     vm2 = {
+#       instance_type = "t2.micro"
+#       key_name      = "tfm-key"
+#       tags = {
+#         Name    = "vm2"
+#         Project = "uber"
+#       }
+#     }
+#   }
+# }
